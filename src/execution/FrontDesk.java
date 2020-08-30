c5e861c549bf575494a6103f441a132526e217d6
divyam1412 <68561075+divyam1412@users.noreply.github.com>
2020-08-30 22:56:07 +0530

    Update FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,13 +1,58 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+/*  User: Divyam goel
+ *  Date: 30/08/20
+ *  Time: 10:50 PM
  *  File Name : FrontDesk.java
  * */
 package execution;
 
+import definitions.Student;
+
+import java.util.Scanner;
+
 public class FrontDesk {
+    private final static int ISSUE_NEW_BOOK = 1;
+    private final static int RETURN_BOOK = 2;
+    private final static int SHOW_ALL_BOOKS = 3;
+    private final static int EXIT = 4;
+
     public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+        Student student = new Student("Divyam", 191500272L, 6);
+        String bookName;
+        int studentInput;
+        do {
+            System.out.println("-=-=--=-=-" + "\"" + "Welcome to the Front Desk" + "\"" + "-=-=--=-=-");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return the previously issued book for me.");
+            System.out.println("3. Show all my issued books.");
+            System.out.println("4. Exit.");
+            System.out.println("Enter your choice (1..4): ");
+            studentInput = scannerObject.nextInt();
+            switch (studentInput) {
+                case ISSUE_NEW_BOOK:
+                    System.out.println("Enter the name of the book you want to issue: ");
+                    scannerObject.nextLine();
+                    bookName = scannerObject.nextLine();
+                    student.issueBook(bookName);
+                    break;
+                case RETURN_BOOK:
+                    System.out.println("Enter the name of the book you want to return: ");
+                    scannerObject.nextLine();
+                    bookName = scannerObject.nextLine();
+                    student.doReturn(bookName);
+                    break;
+                case SHOW_ALL_BOOKS:
+                    student.showAllBooks();
+                    break;
+                default:
+                    break;
+            }
+        } while (studentInput != EXIT);
+        scannerObject.close();
+    }
+}
+
 
     }
 }
