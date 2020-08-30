44e562ea2a9d087227df9d0437ca1aa0dc7c8a5d
divyam1412 <68561075+divyam1412@users.noreply.github.com>
2020-08-30 22:40:26 +0530

    Update Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,50 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+/*  User: Divyam goel
  *  Date: 21/08/20
  *  Time: 3:50 PM
  *  File Name : Library.java
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Library {
+
+
+    private Book[] books;
+
+    public Book[] getBooks() {
+        return books.clone();
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
+    }
+
+    public Library() {
+        this.books = new Book[50];
+        for (int i = 0; i < books.length; i++) {
+            books[i] = new Book("Book " + (i + 1), "Book Author " + (i + 1), Long.toString(1234567000000L + i));
+        }
+
+    }
+    // the 'special' methods are already discussed before.
+
+    @Override
+    public String toString() {
+        return String.format("Books in Library: %s", Arrays.toString(books));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getBooks(), library.getBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getBooks());
+    }
+}
 }
